f2e9e79b9245df788224e75931bd3d9fe655edec
joaopinheir <149428094+joaopinheir@users.noreply.github.com>
2025-05-27 18:55:37 +0000

    add:aula27/05/25

---
+++ src/pages/api/noticias/noticias.js
@@ -0,0 +1,215 @@
+
+
+export const noticias = [
+    {
+        "idnoticia": 1,
+        "titulonoticia": "Notícia sobre pesquisa #1",
+        "conteudonoticia": "Notícia sobre pesquisa #1. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "pesquisa",
+        "datahoracadastro": "2024-08-14T10:12:00"
+    },
+    {
+        "idnoticia": 2,
+        "titulonoticia": "Notícia sobre entretenimento #2",
+        "conteudonoticia": "Notícia sobre entretenimento #2. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "entretenimento",
+        "datahoracadastro": "2024-12-22T14:45:00"
+    },
+    {
+        "idnoticia": 3,
+        "titulonoticia": "Notícia sobre ciência #3",
+        "conteudonoticia": "Notícia sobre ciência #3. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "ciência",
+        "datahoracadastro": "2025-01-10T09:20:00"
+    },
+    {
+        "idnoticia": 4,
+        "titulonoticia": "Notícia sobre cultura #4",
+        "conteudonoticia": "Notícia sobre cultura #4. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "cultura",
+        "datahoracadastro": "2024-11-18T17:00:00"
+    },
+    {
+        "idnoticia": 5,
+        "titulonoticia": "Notícia sobre esportes #5",
+        "conteudonoticia": "Notícia sobre esportes #5. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "esportes",
+        "datahoracadastro": "2024-07-01T11:30:00"
+    },
+    {
+        "idnoticia": 6,
+        "titulonoticia": "Notícia sobre educação #6",
+        "conteudonoticia": "Notícia sobre educação #6. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "educação",
+        "datahoracadastro": "2024-10-02T13:50:00"
+    },
+    {
+        "idnoticia": 7,
+        "titulonoticia": "Notícia sobre ciência #7",
+        "conteudonoticia": "Notícia sobre ciência #7. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "ciência",
+        "datahoracadastro": "2025-01-25T08:10:00"
+    },
+    {
+        "idnoticia": 8,
+        "titulonoticia": "Notícia sobre pesquisa #8",
+        "conteudonoticia": "Notícia sobre pesquisa #8. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "pesquisa",
+        "datahoracadastro": "2024-06-13T15:40:00"
+    },
+    {
+        "idnoticia": 9,
+        "titulonoticia": "Notícia sobre cultura #9",
+        "conteudonoticia": "Notícia sobre cultura #9. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "cultura",
+        "datahoracadastro": "2024-09-05T19:25:00"
+    },
+    {
+        "idnoticia": 10,
+        "titulonoticia": "Notícia sobre entretenimento #10",
+        "conteudonoticia": "Notícia sobre entretenimento #10. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "entretenimento",
+        "datahoracadastro": "2024-08-27T21:55:00"
+    },
+    {
+        "idnoticia": 11,
+        "titulonoticia": "Notícia sobre educação #11",
+        "conteudonoticia": "Notícia sobre educação #11. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "educação",
+        "datahoracadastro": "2024-10-19T09:33:00"
+    },
+    {
+        "idnoticia": 12,
+        "titulonoticia": "Notícia sobre ciência #12",
+        "conteudonoticia": "Notícia sobre ciência #12. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "ciência",
+        "datahoracadastro": "2024-07-30T07:10:00"
+    },
+    {
+        "idnoticia": 13,
+        "titulonoticia": "Notícia sobre cultura #13",
+        "conteudonoticia": "Notícia sobre cultura #13. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "cultura",
+        "datahoracadastro": "2024-09-11T18:15:00"
+    },
+    {
+        "idnoticia": 14,
+        "titulonoticia": "Notícia sobre esportes #14",
+        "conteudonoticia": "Notícia sobre esportes #14. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "esportes",
+        "datahoracadastro": "2024-12-02T12:22:00"
+    },
+    {
+        "idnoticia": 15,
+        "titulonoticia": "Notícia sobre pesquisa #15",
+        "conteudonoticia": "Notícia sobre pesquisa #15. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "pesquisa",
+        "datahoracadastro": "2024-06-22T16:40:00"
+    },
+    {
+        "idnoticia": 16,
+        "titulonoticia": "Notícia sobre entretenimento #16",
+        "conteudonoticia": "Notícia sobre entretenimento #16. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "entretenimento",
+        "datahoracadastro": "2024-11-10T14:50:00"
+    },
+    {
+        "idnoticia": 17,
+        "titulonoticia": "Notícia sobre ciência #17",
+        "conteudonoticia": "Notícia sobre ciência #17. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "ciência",
+        "datahoracadastro": "2024-10-28T06:35:00"
+    },
+    {
+        "idnoticia": 18,
+        "titulonoticia": "Notícia sobre educação #18",
+        "conteudonoticia": "Notícia sobre educação #18. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "educação",
+        "datahoracadastro": "2024-08-06T17:25:00"
+    },
+    {
+        "idnoticia": 19,
+        "titulonoticia": "Notícia sobre cultura #19",
+        "conteudonoticia": "Notícia sobre cultura #19. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "cultura",
+        "datahoracadastro": "2024-09-29T13:05:00"
+    },
+    {
+        "idnoticia": 20,
+        "titulonoticia": "Notícia sobre esportes #20",
+        "conteudonoticia": "Notícia sobre esportes #20. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "esportes",
+        "datahoracadastro": "2024-07-18T08:15:00"
+    },
+    {
+        "idnoticia": 21,
+        "titulonoticia": "Notícia sobre pesquisa #21",
+        "conteudonoticia": "Notícia sobre pesquisa #21. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "pesquisa",
+        "datahoracadastro": "2024-06-04T20:40:00"
+    },
+    {
+        "idnoticia": 22,
+        "titulonoticia": "Notícia sobre entretenimento #22",
+        "conteudonoticia": "Notícia sobre entretenimento #22. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "entretenimento",
+        "datahoracadastro": "2024-12-19T19:30:00"
+    },
+    {
+        "idnoticia": 23,
+        "titulonoticia": "Notícia sobre ciência #23",
+        "conteudonoticia": "Notícia sobre ciência #23. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "ciência",
+        "datahoracadastro": "2024-08-11T22:55:00"
+    },
+    {
+        "idnoticia": 24,
+        "titulonoticia": "Notícia sobre educação #24",
+        "conteudonoticia": "Notícia sobre educação #24. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "educação",
+        "datahoracadastro": "2024-07-05T10:05:00"
+    },
+    {
+        "idnoticia": 25,
+        "titulonoticia": "Notícia sobre cultura #25",
+        "conteudonoticia": "Notícia sobre cultura #25. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "cultura",
+        "datahoracadastro": "2024-09-02T18:45:00"
+    },
+    {
+        "idnoticia": 26,
+        "titulonoticia": "Notícia sobre esportes #26",
+        "conteudonoticia": "Notícia sobre esportes #26. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "esportes",
+        "datahoracadastro": "2024-06-29T16:20:00"
+    },
+    {
+        "idnoticia": 27,
+        "titulonoticia": "Notícia sobre pesquisa #27",
+        "conteudonoticia": "Notícia sobre pesquisa #27. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "pesquisa",
+        "datahoracadastro": "2024-08-14T07:30:00"
+    },
+    {
+        "idnoticia": 28,
+        "titulonoticia": "Notícia sobre entretenimento #28",
+        "conteudonoticia": "Notícia sobre entretenimento #28. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "entretenimento",
+        "datahoracadastro": "2024-10-23T21:45:00"
+    },
+    {
+        "idnoticia": 29,
+        "titulonoticia": "Notícia sobre ciência #29",
+        "conteudonoticia": "Notícia sobre ciência #29. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "ciência",
+        "datahoracadastro": "2024-11-14T12:10:00"
+    },
+    {
+        "idnoticia": 30,
+        "titulonoticia": "Notícia sobre educação #30",
+        "conteudonoticia": "Notícia sobre educação #30. Esta notícia fornece informações detalhadas e relevantes sobre o assunto, com foco em eventos recentes, descobertas ou fatos importantes. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. Ela visa manter o leitor bem informado e atualizado com conteúdo confiável e objetivo. ",
+        "tiponoticia": "educação",
+        "datahoracadastro": "2024-07-12T09:10:00"
+    }
+]
+
--- src/pages/funcaonext/gssp.js
+++ src/pages/funcaonext/gssp.js
@@ -1,20 +1,56 @@
-import { Container } from "react-bootstrap"
+import { Container, Table } from "react-bootstrap"
+import { noticias } from "../api/noticias/noticias"
 
 export async function getServerSideProps() {
-  // Fetch data from external API
-  //const res = await fetch('https://api.github.com/repos/vercel/next.js')
-  //const repo = await res.json()
-  // Pass data to the page via props
-  return { props: { repo: {var1:"Next",var2:"Com React"} } }
+  return {
+    props: {
+      repo: { var1: "Next", var2: "Com React" }
+    }
+  }
 }
- 
+
 export default function Gssp({ repo }) {
   return (
-    <> 
-    <Container>
-       <h1>{repo.var1}</h1>
-       <h1>{repo.var2}</h1>
-    </Container>
+    <>
+      <Container>
+        <h1>{repo.var1}</h1>
+        <h1>{repo.var2}</h1>
+      </Container>
+      <Container>
+        <Table striped bordered hover>
+          <thead className="text-center">
+            <tr>
+              <th>id</th>
+              <th>título</th>
+              <th>conteúdo</th>
+              <th>tipo da notícia</th>
+              <th>d/h</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.isArray(noticias)?
+              noticias.map((noticia) => (
+                <tr key={noticia.idnoticia}>
+                  <td>{noticia.idnoticia}</td>
+                  <td>{noticia.titulonoticia}</td>
+                  <td>{noticia.conteudonoticia}</td>
+                  <td>{noticia.tiponoticia}</td>
+                  <td>{noticia.datahoracadastroa}</td>
+                </tr>
+              )
+            ) : (
+              <tr>
+                <td colSpan="5" className="text-center">Nenhuma notícia disponível</td>
+              </tr>
+            )}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={5}>Info 5C</td>
+            </tr>
+          </tfoot>
+        </Table>
+      </Container>
     </>
   )
 }
