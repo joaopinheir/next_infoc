f021e610cd46476e7b646c7760b9c2e436a5b36a
joaopinheir <149428094+joaopinheir@users.noreply.github.com>
2025-06-16 18:23:05 +0000

    add:aula16/06/25

--- src/pages/noticias/[id].js
+++ src/pages/noticias/[id].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
  
-export default function Page() {
-  const router = useRouter()
-  return <p>id: {router.query.id}</p>
-}
+ export default function Page() {
+   const router = useRouter()
+     return <p>id: {router.query.id}</p>
+ }
---
+++ src/pages/noticias/tipo/[tiponoticia].js
@@ -0,0 +1,13 @@
+import {noticias} from "../noticias"
+
+export default function handler (req,res){
+    console.log(req.query.id);
+    const encontrado = noticias.find((noticia) => noticia.idnoticia.toString() === req.query.tiponoticia);
+
+   // console.log(encontrado);
+    if (!encontrado){
+        return res. status(404).json({error:"noticia nao encontrada"});
+    }
+
+res,status(200).json("encontrado")
+}
